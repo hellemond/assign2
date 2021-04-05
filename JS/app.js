d83102bf23d0af6a52ada46d3a5f6b9957c7eb54
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-04-05 14:40:15 -0700

    note taking area function prepped in js directory

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+/* Function to change theme between light and dark via button press,
+changes background-colour of body tag */
+
+/* Function to create note taking area from clicking new note button.
+- Ask Nathan how note taking area is hidden/revealed. */
+
