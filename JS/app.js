99973632211e0be42af1741f75caf514d2d64800
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-04-05 14:40:47 -0700

    for loop function for save/cancel buttons prepped

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,8 @@ changes background-colour of body tag */
 /* Function to create note taking area from clicking new note button.
 - Ask Nathan how note taking area is hidden/revealed. */
 
+/* For loop function to check if save or cancel is pressed.
+-if save, remove and send text to notes area
+-if cancel, remove note and dont send to notes area */
+
+/* notes area should be an array that saved notes get pushed into. */
